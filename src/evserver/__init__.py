f570e4b3d9640e988835afbd9721080c9f0e14b7
Wannes Vantorre <vantorrewannes@gmail.com>
2026-08-20 20:00:02 +0200

    Add workspace, snapshot, manifest, reference, and content endpoints

--- src/evserver/__init__.py
+++ src/evserver/__init__.py
@@ -126,7 +126,218 @@ async def put_user(
     "/user/{user_id}",
 )
 async def delete_user(user_id: UserId, user_store: UserStoreDependency) -> None:
-    await user_store.delete(user_id)
+    if user_store.contains(user_id):
+        await user_store.delete(user_id)
+
+
+@application.head(
+    "/workspace/{workspace_id}",
+)
+async def head_workspace(
+    workspace_id: WorkspaceId, workspace_store: WorkspaceStoreDependency
+) -> None:
+    if not await workspace_store.contains(workspace_id):
+        raise HTTPException(404)
+
+
+@application.get(
+    "/workspace/{workspace_id}",
+)
+async def get_workspace(
+    workspace_id: WorkspaceId, workspace_store: WorkspaceStoreDependency
+) -> Workspace:
+    if await workspace_store.contains(workspace_id):
+        return await workspace_store.get(workspace_id)
+    raise HTTPException(404)
+
+
+@application.put(
+    "/workspace/{workspace_id}",
+)
+async def put_workspace(
+    workspace_id: WorkspaceId,
+    workspace: Workspace,
+    workspace_store: WorkspaceStoreDependency,
+) -> None:
+    await workspace_store.set(workspace_id, workspace)
+
+
+@application.delete(
+    "/workspace/{workspace_id}",
+)
+async def delete_workspace(
+    workspace_id: WorkspaceId, workspace_store: WorkspaceStoreDependency
+) -> None:
+    if workspace_store.contains(workspace_id):
+        await workspace_store.delete(workspace_id)
+
+
+@application.head(
+    "/snapshot/{snapshot_id}",
+)
+async def head_snapshot(
+    snapshot_id: SnapshotId, snapshot_store: SnapshotDependency
+) -> None:
+    if not await snapshot_store.contains(snapshot_id):
+        raise HTTPException(404)
+
+
+@application.get(
+    "/snapshot/{snapshot_id}",
+)
+async def get_snapshot(
+    snapshot_id: SnapshotId, snapshot_store: SnapshotDependency
+) -> Snapshot:
+    if await snapshot_store.contains(snapshot_id):
+        return await snapshot_store.get(snapshot_id)
+    raise HTTPException(404)
+
+
+@application.put(
+    "/snapshot/{snapshot_id}",
+)
+async def put_snapshot(
+    snapshot_id: SnapshotId,
+    snapshot: Snapshot,
+    snapshot_store: SnapshotDependency,
+) -> None:
+    await snapshot_store.set(snapshot_id, snapshot)
+
+
+@application.delete(
+    "/snapshot/{snapshot_id}",
+)
+async def delete_snapshot(
+    snapshot_id: SnapshotId, snapshot_store: SnapshotDependency
+) -> None:
+    if snapshot_store.contains(snapshot_id):
+        await snapshot_store.delete(snapshot_id)
+
+
+@application.head(
+    "/manifest/{manifest_id}",
+)
+async def head_manifest(
+    manifest_id: ManifestId, manifest_store: ManifestStoreDependency
+) -> None:
+    if not await manifest_store.contains(manifest_id):
+        raise HTTPException(404)
+
+
+@application.get(
+    "/manifest/{manifest_id}",
+)
+async def get_manifest(
+    manifest_id: ManifestId, manifest_store: ManifestStoreDependency
+) -> Manifest:
+    if await manifest_store.contains(manifest_id):
+        return await manifest_store.get(manifest_id)
+    raise HTTPException(404)
+
+
+@application.put(
+    "/manifest/{manifest_id}",
+)
+async def put_manifest(
+    manifest_id: ManifestId,
+    manifest: Manifest,
+    manifest_store: ManifestStoreDependency,
+) -> None:
+    await manifest_store.set(manifest_id, manifest)
+
+
+@application.delete(
+    "/manifest/{manifest_id}",
+)
+async def delete_manifest(
+    manifest_id: ManifestId, manifest_store: ManifestStoreDependency
+) -> None:
+    if manifest_store.contains(manifest_id):
+        await manifest_store.delete(manifest_id)
+
+
+@application.head(
+    "/reference/{reference_id}",
+)
+async def head_reference(
+    reference_id: ReferenceId, reference_store: ReferenceStoreDependency
+) -> None:
+    if not await reference_store.contains(reference_id):
+        raise HTTPException(404)
+
+
+@application.get(
+    "/reference/{reference_id}",
+)
+async def get_reference(
+    reference_id: ReferenceId, reference_store: ReferenceStoreDependency
+) -> Reference:
+    if await reference_store.contains(reference_id):
+        return await reference_store.get(reference_id)
+    raise HTTPException(404)
+
+
+@application.put(
+    "/reference/{reference_id}",
+)
+async def put_reference(
+    reference_id: ReferenceId,
+    reference: Reference,
+    reference_store: ReferenceStoreDependency,
+) -> None:
+    await reference_store.set(reference_id, reference)
+
+
+@application.delete(
+    "/reference/{reference_id}",
+)
+async def delete_reference(
+    reference_id: ReferenceId, reference_store: ReferenceStoreDependency
+) -> None:
+    if reference_store.contains(reference_id):
+        await reference_store.delete(reference_id)
+
+
+@application.head(
+    "/content/{content_id}",
+)
+async def head_content(
+    content_id: ContentId, content_store: ContentStoreDependency
+) -> None:
+    if not await content_store.contains(content_id):
+        raise HTTPException(404)
+
+
+@application.get(
+    "/content/{content_id}",
+)
+async def get_content(
+    content_id: ContentId, content_store: ContentStoreDependency
+) -> Content:
+    if await content_store.contains(content_id):
+        return await content_store.get(content_id)
+    raise HTTPException(404)
+
+
+@application.put(
+    "/content/{content_id}",
+)
+async def put_content(
+    content_id: ContentId,
+    content: Content,
+    content_store: ContentStoreDependency,
+) -> None:
+    await content_store.set(content_id, content)
+
+
+@application.delete(
+    "/content/{content_id}",
+)
+async def delete_content(
+    content_id: ContentId, content_store: ContentStoreDependency
+) -> None:
+    if content_store.contains(content_id):
+        await content_store.delete(content_id)
 
 
 def main() -> None:
